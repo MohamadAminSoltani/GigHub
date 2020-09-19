b0f7ae0ac450dbd68902a49fe93af1f8db0ae6d2
MohamadAminSoltani <maxmadmas@gmail.com>
2020-09-19 23:55:56 +0430

    Implement anti-forgery token

--- GigHub/ViewModels/ValidTime.cs
+++ GigHub/ViewModels/ValidTime.cs
@@ -9,7 +9,7 @@ namespace GigHub.ViewModels
         public override bool IsValid(object value)
         {
             DateTime dateTime;
-            var isValid = DateTime.TryParseExact(Convert.ToString(value), "HHmm", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime);
+            var isValid = DateTime.TryParseExact(Convert.ToString(value), "HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime);
 
             return (isValid);
         }
